f2a7d53398e7b99fcd851870127281184ddccd34
Monique Aguiar <monikeagn@gmail.com>
2025-02-20 17:47:33 -0300

    App

---
+++ app/src/main/java/com/example/appfrasesdodia/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.appfrasesdodia;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Atributo
+    String[] frases = {
+            "Você está no caminho certo",
+            "Tenha foco. Não se desvie",
+            "A vida tem que ser leve!",
+            "Em tudo Deus vai te ajudar, basta você rezar!",
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void gerarFrase(View view) {
+
+
+
+    }
+
+    public void exibirTodas(View view) {
+        for (String frase : frases) {
+            System.out.println(frase);
+
+        }
+
+
+    }
+
+
+
+
+
+}
